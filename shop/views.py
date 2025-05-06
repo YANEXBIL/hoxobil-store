3f7cea2c5fb4dc3962dfc37904105b48000fdffd
yanexbil <yanex66.ii@gmail.com>
2025-05-06 17:18:52 +0100

    Initial commit: HOXOBIL e-commerce setup with .gitignore

---
+++ shop/views.py
@@ -0,0 +1,8 @@
+# shop/views.py
+
+from django.shortcuts import render
+from .models import Product
+
+def home(request):
+    products = Product.objects.all()
+    return render(request, 'shop/home.html', {'products': products})
